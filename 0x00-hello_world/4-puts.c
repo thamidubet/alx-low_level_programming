d5b6a45b0d3c6f922a86e4f996771f360e3e5469
thamidubet <thamidubet@gmail.com>
2022-09-12 15:23:21 -0700

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+  
+ /** 
+  * main - Entry point 
+  * Description: prints "\"Programming is like building a multilingual puzzle" 
+  * Return: 0 
+  */ 
+  
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+         return (0); 
+ }
+
+5-printf.c
+#include <stdio.h> 
+  
+ /** 
+  * main - Print a string 
+  * Description: use printf 
+  * Return: 0 
+  */ 
