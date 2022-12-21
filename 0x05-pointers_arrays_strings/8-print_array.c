dc6901c5e71443178e1b8436bfb7aaf2b6e3bad0
thamidubet <thamidubet@gmail.com>
2022-12-20 19:21:02 -0800

    updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,21 @@
-#include "main.h" 
-  
- /** 
-  * main - check the code for ALX School students. 
-  * 
-  * Return: Always 0. 
-  */ 
- int main(void) 
- { 
-         int array[5]; 
-  
-         array[0] = 98; 
-         array[1] = 402; 
-         array[2] = -198; 
-         array[3] = 298; 
-         array[4] = -1024; 
-         print_array(array, 5); 
-         return (0); 
- }
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: input array.
+ * @n: input n elements
+ * Return: no return.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
